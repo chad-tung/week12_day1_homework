da7048b5955a200b6f424464db1efbebb57cfd09
chad-tung <chad.tung@gmail.com>
2017-12-04 17:28:21 +0000

    Homework done

--- public/app.js
+++ public/app.js
@@ -4,28 +4,29 @@ var app = function() {
     var catUl = document.createElement('ul');
     catUl.classList.add('cat');
     return catUl;
-  }
+  };
 
   var createCatName = function(name) {
     var catName = document.createElement('li');
     catName.innerText = `Name: ${name}`;
     return catName;
-  }
+  };
 
   var createCatFaveFood = function(food) {
     var catFaveFood = document.createElement('li');
     catFaveFood.innerText = `Favourite food: ${food}`;
     return catFaveFood;
-  }
+  };
 
   var createCatPic = function(pic) {
     var catPic = document.createElement('li');
+    catPic.classList.add('picture');
     var picture = document.createElement('img');
-    picture.width = 500;
+    picture.width = '500';
     picture.src = pic;
     catPic.appendChild(picture);
     return catPic;
-  }
+  };
 
   var appendElements = function(name, food, pic) {
     var catUl = createCatUl();
@@ -36,7 +37,7 @@ var app = function() {
 
     var cats = document.querySelector('#cats');
     cats.appendChild(catUl);
-  }
+  };
 
   var createCat = function(name, food, pic) {
     var catName = createCatName(name);
@@ -50,11 +51,11 @@ var app = function() {
     this.name = name;
     this.food = food;
     this.pic = pic;
-  }
+  };
 
   var easyCreateCat = function(cat) {
     createCat(cat.name, cat.food, cat.pic);
-  }
+  };
 
 
   var cat1 = new Cat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
@@ -65,13 +66,13 @@ var app = function() {
 
   var cat4 = new Cat("Chad", "Duck", 'http://canitbesaturdaynow.com/images/fpics/530/4_10_09_%20(67).jpg');
 
-  var catArr = [cat1, cat2, cat3, cat4]
+  var catArr = [cat1, cat2, cat3, cat4];
 
   for (cat of catArr) {
     easyCreateCat(cat);
-  }
+  };
 
-}
+};
 
 
 
