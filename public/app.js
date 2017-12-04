9c2a2cf7104428b26cb03d842b2520f533c4524b
chad-tung <chad.tung@gmail.com>
2017-12-04 15:46:37 +0000

    Trying to make the create functions

--- public/app.js
+++ public/app.js
@@ -1,5 +1,29 @@
 var app = function() {
 
+  var createCatUl = function(){
+    var catUl = document.createElement('ul');
+    catArticle.classList.add('cat');
+    return catUl;
+  }
+
+  var createCatName = function(name) {
+    car catName = document.createElement('li');
+    catName.innerText = `Name: ${name}`;
+    return catName;
+  }
+
+  var createCatFaveFood = function(food) {
+    var catFaveFood = document.createElement('li');
+    catFaveFood.innerText = `Favourite food: ${food}`;
+    return catFaveFood;
+  }
+
+  var createCatPic = function(pic) {
+    var catPic = document.createElement('li');
+    var picture = document.createElement('img');
+  }
+
+
 }
 
 window.onload = app;
