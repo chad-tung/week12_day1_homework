11e640ced2e7d063cce395669a24e539eb86c41c
chad-tung <chad.tung@gmail.com>
2017-12-04 16:52:02 +0000

    Created cat constructor

--- public/app.js
+++ public/app.js
@@ -46,7 +46,27 @@ var app = function() {
     appendElements(catName, catFood, catPic);
   }
 
+  var Cat = function(name, food, pic) {
+    this.name = name;
+    this.food = food;
+    this.pic = pic;
+  }
+
+  var easyCreateCat = function(cat) {
+    createCat(cat.name, cat.food, cat.pic);
+  }
+
+
+
   createCat("Chad", "Duck", 'http://canitbesaturdaynow.com/images/fpics/530/4_10_09_%20(67).jpg');
+
+  var cat1 = new Cat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+
+  var cat2 = new Cat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+
+  var cat3 = new Cat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
+
+  var cat3 = new Cat("Chad", "Duck", 'http://canitbesaturdaynow.com/images/fpics/530/4_10_09_%20(67).jpg');
 
 }
 
